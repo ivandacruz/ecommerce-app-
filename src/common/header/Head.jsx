1018c21ff9c72c5644e478c764b76309775f600d
Ivan da Cruz <64236894+ivandacruz@users.noreply.github.com>
2023-02-27 20:32:54 -0300

    feat: add head header navbar search

---
+++ src/common/header/Head.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Head = () => {
+  return (
+    <>
+        <section className='head'>
+          <div className='container d_flex'>
+            <div className='left row'>
+              <i className='fa fa-phone'></i>
+              <label htmlFor="">+55 11 95550 9090</label>
+            </div>
+          </div>
+        </section>
+    </>
+  )
+}
+
+export default Head
